91abf1c249d389aac36690773f31e0b1ea12c3cb
Victor Fernandez <108770372+VictorFernandezArias@users.noreply.github.com>
2025-08-11 23:25:33 -0400

    Add simple REST endpoint to store users

--- src/main/java/com/example/MainApp.java
+++ src/main/java/com/example/MainApp.java
@@ -1,22 +1,11 @@
 package com.example;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.main.Main;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class MainApp {
-
-    public static void main(String[] args) throws Exception {
-        Main main = new Main();
-        main.configure().addRoutesBuilder(new RouteBuilder() {
-            @Override
-            public void configure() {
-                from("timer:pokemon?repeatCount=1")
-                    .setHeader(Exchange.HTTP_METHOD, constant("GET"))
-                    .to("https://pokeapi.co/api/v2/pokemon/ditto")
-                    .to("file:output?fileName=pokemon.json");
-            }
-        });
-        main.run(args);
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp.class, args);
     }
 }
